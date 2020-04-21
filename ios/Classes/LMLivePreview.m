665593e1a1d1b286cc8f257a84c9316a0fb6e3fe
mrysak <martyna@zappmeet.com>
2020-04-21 13:06:54 +0200

    refactoring, felxible label, images

--- ios/Classes/LMLivePreview.m
+++ ios/Classes/LMLivePreview.m
@@ -49,7 +49,6 @@
     AVAuthorizationStatus status = [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo];
     switch (status) {
         case AVAuthorizationStatusNotDetermined:{
-            // 许可对话没有出现，发起授权许可
             [AVCaptureDevice requestAccessForMediaType:AVMediaTypeVideo completionHandler:^(BOOL granted) {
                 if (granted) {
                     dispatch_async(dispatch_get_main_queue(), ^{
@@ -60,7 +59,6 @@
             break;
         }
         case AVAuthorizationStatusAuthorized:{
-            // 已经开启授权，可继续
             //dispatch_async(dispatch_get_main_queue(), ^{
             [_self.session setRunning:YES];
             //});
@@ -68,8 +66,6 @@
         }
         case AVAuthorizationStatusDenied:
         case AVAuthorizationStatusRestricted:
-            // 用户明确地拒绝授权，或者相机设备无法访问
-            
             break;
         default:
             break;
@@ -119,6 +115,9 @@
         default:
             break;
     }
+    CGRect labelFrame = [_stateLabel frame];
+    labelFrame.size.width = _stateLabel.intrinsicContentSize.width;
+    [_stateLabel setFrame:labelFrame];
 }
 
 /** live debug info callback */
@@ -134,99 +133,7 @@
 #pragma mark -- Getter Setter
 - (LFLiveSession*)session{
     if(!_session){
-        /**      发现大家有不会用横屏的请注意啦，横屏需要在ViewController  supportedInterfaceOrientations修改方向  默认竖屏  ****/
-        /**      发现大家有不会用横屏的请注意啦，横屏需要在ViewController  supportedInterfaceOrientations修改方向  默认竖屏  ****/
-        /**      发现大家有不会用横屏的请注意啦，横屏需要在ViewController  supportedInterfaceOrientations修改方向  默认竖屏  ****/
-        
-        
-        
-        /***   默认分辨率368 ＊ 640  音频：44.1 iphone6以上48  双声道  方向竖屏 ***/
-        
         _session = [[LFLiveSession alloc] initWithAudioConfiguration:[LFLiveAudioConfiguration defaultConfiguration] videoConfiguration:[LFLiveVideoConfiguration defaultConfigurationForQuality:LFLiveVideoQuality_Medium2]];
-        
-        
-        /**    自己定制单声道  */
-        /*
-         LFLiveAudioConfiguration *audioConfiguration = [LFLiveAudioConfiguration new];
-         audioConfiguration.numberOfChannels = 1;
-         audioConfiguration.audioBitrate = LFLiveAudioBitRate_64Kbps;
-         audioConfiguration.audioSampleRate = LFLiveAudioSampleRate_44100Hz;
-         _session = [[LFLiveSession alloc] initWithAudioConfiguration:audioConfiguration videoConfiguration:[LFLiveVideoConfiguration defaultConfiguration]];
-         */
-        
-        /**    自己定制高质量音频96K */
-        /*
-         LFLiveAudioConfiguration *audioConfiguration = [LFLiveAudioConfiguration new];
-         audioConfiguration.numberOfChannels = 2;
-         audioConfiguration.audioBitrate = LFLiveAudioBitRate_96Kbps;
-         audioConfiguration.audioSampleRate = LFLiveAudioSampleRate_44100Hz;
-         _session = [[LFLiveSession alloc] initWithAudioConfiguration:audioConfiguration videoConfiguration:[LFLiveVideoConfiguration defaultConfiguration]];
-         */
-        
-        /**    自己定制高质量音频96K 分辨率设置为540*960 方向竖屏 */
-        
-        /*
-         LFLiveAudioConfiguration *audioConfiguration = [LFLiveAudioConfiguration new];
-         audioConfiguration.numberOfChannels = 2;
-         audioConfiguration.audioBitrate = LFLiveAudioBitRate_96Kbps;
-         audioConfiguration.audioSampleRate = LFLiveAudioSampleRate_44100Hz;
-         
-         LFLiveVideoConfiguration *videoConfiguration = [LFLiveVideoConfiguration new];
-         videoConfiguration.videoSize = CGSizeMake(540, 960);
-         videoConfiguration.videoBitRate = 800*1024;
-         videoConfiguration.videoMaxBitRate = 1000*1024;
-         videoConfiguration.videoMinBitRate = 500*1024;
-         videoConfiguration.videoFrameRate = 24;
-         videoConfiguration.videoMaxKeyframeInterval = 48;
-         videoConfiguration.orientation = UIInterfaceOrientationPortrait;
-         videoConfiguration.sessionPreset = LFCaptureSessionPreset540x960;
-         
-         _session = [[LFLiveSession alloc] initWithAudioConfiguration:audioConfiguration videoConfiguration:videoConfiguration];
-         */
-        
-        
-        /**    自己定制高质量音频128K 分辨率设置为720*1280 方向竖屏 */
-        
-        /*
-         LFLiveAudioConfiguration *audioConfiguration = [LFLiveAudioConfiguration new];
-         audioConfiguration.numberOfChannels = 2;
-         audioConfiguration.audioBitrate = LFLiveAudioBitRate_128Kbps;
-         audioConfiguration.audioSampleRate = LFLiveAudioSampleRate_44100Hz;
-         
-         LFLiveVideoConfiguration *videoConfiguration = [LFLiveVideoConfiguration new];
-         videoConfiguration.videoSize = CGSizeMake(720, 1280);
-         videoConfiguration.videoBitRate = 800*1024;
-         videoConfiguration.videoMaxBitRate = 1000*1024;
-         videoConfiguration.videoMinBitRate = 500*1024;
-         videoConfiguration.videoFrameRate = 15;
-         videoConfiguration.videoMaxKeyframeInterval = 30;
-         videoConfiguration.orientation = UIInterfaceOrientationPortrait;
-         videoConfiguration.sessionPreset = LFCaptureSessionPreset720x1280;
-         
-         _session = [[LFLiveSession alloc] initWithAudioConfiguration:audioConfiguration videoConfiguration:videoConfiguration];
-         */
-        
-        
-        /**    自己定制高质量音频128K 分辨率设置为720*1280 方向横屏  */
-        
-        /*
-         LFLiveAudioConfiguration *audioConfiguration = [LFLiveAudioConfiguration new];
-         audioConfiguration.numberOfChannels = 2;
-         audioConfiguration.audioBitrate = LFLiveAudioBitRate_128Kbps;
-         audioConfiguration.audioSampleRate = LFLiveAudioSampleRate_44100Hz;
-         
-         LFLiveVideoConfiguration *videoConfiguration = [LFLiveVideoConfiguration new];
-         videoConfiguration.videoSize = CGSizeMake(1280, 720);
-         videoConfiguration.videoBitRate = 800*1024;
-         videoConfiguration.videoMaxBitRate = 1000*1024;
-         videoConfiguration.videoMinBitRate = 500*1024;
-         videoConfiguration.videoFrameRate = 15;
-         videoConfiguration.videoMaxKeyframeInterval = 30;
-         videoConfiguration.landscape = YES;
-         videoConfiguration.sessionPreset = LFCaptureSessionPreset720x1280;
-         
-         _session = [[LFLiveSession alloc] initWithAudioConfiguration:audioConfiguration videoConfiguration:videoConfiguration];
-         */
         
         _session.delegate = self;
         _session.preView = self;
@@ -250,6 +157,10 @@
         _stateLabel.text = @"Not Connected";
         _stateLabel.textColor = [UIColor whiteColor];
         _stateLabel.font = [UIFont boldSystemFontOfSize:14.f];
+
+        CGRect labelFrame = [_stateLabel frame];
+        labelFrame.size.width = _stateLabel.intrinsicContentSize.width;
+        [_stateLabel setFrame:labelFrame];
     }
     return _stateLabel;
 }
@@ -262,10 +173,10 @@
         _closeButton.top = 20;
         [_closeButton setImage:[UIImage imageNamed:@"close_preview"] forState:UIControlStateNormal];
         _closeButton.exclusiveTouch = YES;
-//         [_closeButton addBlockForControlEvents:UIControlEventTouchUpInside block:^(id sender) {
-//             NSLog(@"Close pressed");
-//             [self.viewController dismissViewControllerAnimated:YES completion:nil];
-//         }];
+        [_closeButton addBlockForControlEvents:UIControlEventTouchUpInside block:^(id sender) {
+            NSLog(@"Close pressed");
+            [self.viewController dismissViewControllerAnimated:YES completion:nil];
+        }];
     }
     return _closeButton;
 }
@@ -275,7 +186,7 @@
         _cameraButton = [UIButton new];
         _cameraButton.size = CGSizeMake(44, 44);
         _cameraButton.origin = CGPointMake(_closeButton.left - 10 - _cameraButton.width, 20);
-        [_cameraButton setImage:[UIImage imageNamed:@"camra_preview"] forState:UIControlStateNormal];
+        [_cameraButton setImage:[UIImage imageNamed:@"camera_preview"] forState:UIControlStateNormal];
         _cameraButton.exclusiveTouch = YES;
         __weak typeof(self) _self = self;
         [_cameraButton addBlockForControlEvents:UIControlEventTouchUpInside block:^(id sender) {
@@ -291,8 +202,8 @@
         _beautyButton = [UIButton new];
         _beautyButton.size = CGSizeMake(44, 44);
         _beautyButton.origin = CGPointMake(_cameraButton.left - 10 - _beautyButton.width,20);
-        [_beautyButton setImage:[UIImage imageNamed:@"camra_beauty"] forState:UIControlStateSelected];
-        [_beautyButton setImage:[UIImage imageNamed:@"camra_beauty_close"] forState:UIControlStateNormal];
+        [_beautyButton setImage:[UIImage imageNamed:@"camera_beauty"] forState:UIControlStateSelected];
+        [_beautyButton setImage:[UIImage imageNamed:@"camera_beauty_close"] forState:UIControlStateNormal];
         _beautyButton.exclusiveTouch = YES;
         __weak typeof(self) _self = self;
         [_beautyButton addBlockForControlEvents:UIControlEventTouchUpInside block:^(id sender) {
@@ -322,7 +233,6 @@
                 [_self.startLiveButton setTitle:@"End live" forState:UIControlStateNormal];
                 LFLiveStreamInfo *stream = [LFLiveStreamInfo new];
                 stream.url = self.streamUrl;
-                //stream.url = @"rtmp://daniulive.com:1935/live/stream2399";
                 [_self.session startLive:stream];
             }else{
                 [_self.startLiveButton setTitle:@"Start Live" forState:UIControlStateNormal];
@@ -334,3 +244,4 @@
 }
 
 @end
+
